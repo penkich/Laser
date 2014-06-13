283243a8fc8f49d519e921b1b3f707bbc1dbc82d
penkich <nishijim2001@yahoo.co.jp>
2014-06-14 07:26:46 +0900

    Create mkflame.py

---
+++ mkflame.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pickle
+import sys
+import re
+import math
+import lpenkichsvgcl3 as pensvg
+
+argvs = sys.argv
+argc = len(argvs)
+tmp2 =[]
+for i in range(argc-2):
+	a=pensvg.inksvg(argvs[i+1])
+	b=pensvg.inksvg(argvs[i+2])
+
+	a_structPath = a.getstructPath()
+	a_Matrix = a.getmatrix()
+	a_Translate = a.gettranslate()
+	a_Trcxy = a.gettrcxy()
+	b_structPath = b.getstructPath()
+	b_Matrix = b.getmatrix()
+	b_Translate = b.gettranslate()
+	b_Trcxy = b.gettrcxy()
+
+	tmp =[]
+	n = 30 # n of flames
+	for i in range(n):
+		c=pensvg.devidestructPath(pensvg.touitustructPath(a_structPath),pensvg.touitustructPath(b_structPath),1.0/(n-1) * i,a_Matrix,b_Matrix,a_Translate,b_Translate)
+		d= pensvg.getpoint(c,a_Matrix,a_Trcxy,a_Translate,20)
+		tmp.append(d)
+	tmp2 += tmp
+print pickle.dumps(tmp2)
