8429b130ede8f825af0914b0fe3c72e5a1cc47dc
penkich <nishijim2001@yahoo.co.jp>
2014-06-14 07:32:39 +0900

    Update mk6wav.py

--- mk6wav.py
+++ mk6wav.py
@@ -9,23 +9,23 @@ import sys
 import struct
 import string
 import math
+import numpy as np
 import pickle
 
 argvs = sys.argv
 argc = len(argvs)
 
 fi = sys.stdin
-ar = pickle.load(fi)
+flames = np.array(pickle.load(fi))
 
 #fi = open(argvs[1],"r") # input file of x,y
 fo = open(argvs[1],"wb") # output file of wav
-t = int(argvs[2]) # playing time ... 2000 = ca 5sec
-repeat = int(argvs[3]) # 
+repeat = int(argvs[2]) # redundancy
 
 #################################
 # to make wav header (Extensible Format)
 #################################
-dsize = 2399324-44 # data size (enough size for easy)
+dsize = 20000000 # data size (set enough size for easy)
 fsize = dsize + 44 # wav file size
 cksize = 40 # Chunk size: 40=fix
 fbit = 16 # bits 
@@ -47,7 +47,7 @@ fo.write(struct.pack("H",bz))
 fo.write(struct.pack("H",16))
 fo.write(struct.pack("H",0x0016)) # 22=fix
 fo.write(struct.pack("H",0x0000))
-fo.write(struct.pack("H",0x003F)) # Speaker position mask
+fo.write(struct.pack("H",0x003F)) # Speaker position mask (111111b = 3F)
 fo.write(struct.pack("H",0x0000)) # 
 fo.write(struct.pack("H",0x0001)) # GUID (first two bytes are the data format code)
 fo.write(struct.pack("H",0x0000)) #
@@ -63,14 +63,19 @@ fo.write(struct.pack("i",dsize))
 # x,y,z data followed by the above headder
 #################################
 
+xsize = 750.0
+ysize = 750.0
 
-for x in ar:
-        fo.write(struct.pack("h",x[0])) # ch1 (x)
-        fo.write(struct.pack("h",x[1])) # ch2 (y)
-        fo.write(struct.pack("h",x[2])) # ch3 (z)
-        fo.write(struct.pack("h",0)) # ch4
-        fo.write(struct.pack("h",0)) # ch5
-        fo.write(struct.pack("h",0)) # ch6
+for flame in flames:
+        for path in flame:
+		for x in path:
+			for i in range(repeat):
+				fo.write(struct.pack("h",(x[0]-xsize/2.0)/xsize * 32000)) # ch1 (x)
+				fo.write(struct.pack("h",(x[1]-ysize/2.0)/ysize * 32000)) # ch2 (y)
+				fo.write(struct.pack("h",x[2])) # ch3 (z)
+				fo.write(struct.pack("h",0)) # ch4 not use
+				fo.write(struct.pack("h",0)) # ch5 not use
+				fo.write(struct.pack("h",0)) # ch6 not use
 
 fi.close()
 fo.close()
