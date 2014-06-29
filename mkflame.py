6e0907f811671da753f239d14aeef742896681b2
penkich <nishijim2001@yahoo.co.jp>
2014-06-29 15:23:39 +0900

    Update mkflame.py

--- mkflame.py
+++ mkflame.py
@@ -13,27 +13,29 @@ for i in range(argc-2):
 	a=pensvg.inksvg(argvs[i+1])
 	b=pensvg.inksvg(argvs[i+2])
 
-#	print a.getd()
-
 	a_structPath = a.getstructPath()
 
 	a_gMatrix = a.getgmatrix()
 	a_Matrix = a.getmatrix()
 	a_Translate = a.gettranslate()
-#	a_Trcxy = a.gettrcxy()
-	a_Trcxy = ""
-
-
 
 	b_structPath = b.getstructPath()
 	b_gMatrix = b.getgmatrix()
 	b_Matrix = b.getmatrix()
 	b_Translate = b.gettranslate()
-#	b_Trcxy = b.gettrcxy()
-	b_Trcxy = ""
 
-#	print a_structPath
 
+	GTrans = a.getGTransform()[0]
+	if GTrans[0:9] == 'translate':
+		tmp = GTrans[10:-1]
+		print "translate",GTrans,tmp
+	if GTrans[0:6] == 'matrix':
+		print "matrix",GTrans
+	if GTrans[0:5] == 'scale':
+		tmp = GTrans[6:-1]
+		print "scale",GTrans,tmp
+
+	a_color = a.getcolor()
 
 	if len(a_gMatrix) >1:
 		if a_gMatrix[1]:
@@ -60,18 +62,17 @@ for i in range(argc-2):
 #	print argvs[i+1]
 #	print a_Matrix
 #	print a_Translate
-#	print a_Trcxy
 
 #	print argvs[i+2]
 #	print b_Matrix
 #	print b_Translate
-#	print b_Trcxy
+
 
 	tmp =[]
 	n = 30 # n of flames
 	for i in range(n):
 		c= pensvg.devidestructPath(pensvg.touitustructPath(a_structPath),pensvg.touitustructPath(b_structPath),1.0/(n-1) * i,a_Matrix,b_Matrix,a_Translate,b_Translate)
-		d= pensvg.getpoint(c,a_Matrix,a_Trcxy,a_Translate,20)
+		d= pensvg.getpoint(c,a_Matrix,a_Translate,a_color,20)
 		tmp.append(d)
 	tmp2 += tmp
 #print tmp2
